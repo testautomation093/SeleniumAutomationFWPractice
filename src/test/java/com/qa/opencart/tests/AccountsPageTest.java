bbf658a008581990fdd1e63330d00b9ac023f0e8
Nishant Goel <testautomation093@gmail.com>
2025-12-26 13:34:09 +0530

    Changes done to check the NGROK proxy

--- src/test/java/com/qa/opencart/tests/AccountsPageTest.java
+++ src/test/java/com/qa/opencart/tests/AccountsPageTest.java
@@ -10,38 +10,32 @@ import com.qa.opencart.base.BaseTest;
 import com.qa.opencart.constants.AppConstants;
 
 public class AccountsPageTest extends BaseTest {
-	
+
 	@BeforeClass
-	public void accSetUp()
-	{
-		accPage=loginPage.doLogin(prop.getProperty("username"),prop.getProperty("password"));
-		//accPage=new AccountsPage(driver);
-		
-	}
-	
-	@Test
-	public void isLogOutLinkExistTest()
-	{
-		boolean flag=accPage.isLogOutLinkExist();
-		Assert.assertTrue(flag);
-	}
-	
-	@Test
-	public void accPageHeadersTest()
-	{
-		List<String> listHeaders=accPage.getAccPageHeaders();
-		Assert.assertEquals(listHeaders.size(),4);
-		
-		Assert.assertEquals(listHeaders,AppConstants.expHeaders);
-		
-	}
-	
-	
-	@Test
-	public void searchProductTest()
-	{
-		accPage.doSearch("imac");
+	public void accSetUp() {
+		accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+		// accPage=new AccountsPage(driver);
+
 	}
 
+	@Test
+	public void isLogOutLinkExistTest() {
+		boolean flag = accPage.isLogOutLinkExist();
+		Assert.assertTrue(flag);
+	}
+
+	@Test
+	public void accPageHeadersTest() {
+		List<String> listHeaders = accPage.getAccPageHeaders();
+		Assert.assertEquals(listHeaders.size(), 4);
+
+		Assert.assertEquals(listHeaders, AppConstants.expHeaders);
+
+	}
+
+	@Test
+	public void searchProductTest() {
+		accPage.doSearch("imac");
+	}
 
 }
